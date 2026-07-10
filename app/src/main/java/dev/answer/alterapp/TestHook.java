2bc1ef287c10996ca3b4c812b3cd25cb234ad4f1
answer2 <2903536884@qq.com>
2026-07-10 23:46:38 +0800

    [Update] add license and remove file

---
+++ app/src/main/java/dev/answer/alterapp/TestHook.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2026 AnswerDev
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by AnswerDev
+ */
+package dev.answer.alterapp;
+
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+import dev.answer.altercore.AlterCore;
+import dev.answer.altercore.callback.MethodHook;
+import dev.answer.altercore.callback.MethodReplacement;
+import dev.answer.altercore.core.HookParams;
+
+public class TestHook {
+
+    private static final String TAG = "TestHook";
+
+    public String a(String aaa, String aab) {
+        return "a" + aaa + aab;
+    }
+
+    public static String b() {
+        return "origin b";
+    }
+
+    public static void test() {
+        try {
+            Method a_method = TestHook.class.getMethod("a", String.class, String.class);
+            Method b_method = TestHook.class.getMethod("b");
+
+            MethodHook.Unhook un = AlterCore.hook(a_method, new MethodHook() {
+                @Override
+                public void after(HookParams params) throws Throwable {
+                    super.after(params);
+                    params.setResult("你好哇");
+                }
+
+                @Override
+                public void before(HookParams params) throws Throwable {
+                    super.before(params);
+
+                }
+            });
+
+            MethodHook.Unhook un_ = AlterCore.hook(b_method, new MethodReplacement() {
+                @Override
+                protected Object replace(HookParams params) throws Throwable {
+                    return "Replace b";
+                }
+            });
+
+            var test = new TestHook();
+
+            Log.d(TAG,"Hook : " +  test.a(" Hello", "114514"));
+
+            un.unhook();
+
+            Log.d(TAG,"Origin : " +  test.a(" Hello", "114514"));
+
+
+            Log.d(TAG,"Hook : " +  b());
+
+            un_.unhook();
+
+            Log.d(TAG,"Origin : " +  b());
+
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
